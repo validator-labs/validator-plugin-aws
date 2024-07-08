841cb31542da49a31b51142108f39aef4d91d17a
Tyler Gillson <tyler.gillson@gmail.com>
2024-07-08 08:56:48 -0600

    refactor: enable revive & resolve all lints (#420)
    ## Issue
    <!-- Link to the github issue this PR address, ie: #123 -->
    
    ## Description
    <!-- Description of the changes made -->
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

--- internal/controller/awsvalidator_controller_test.go
+++ internal/controller/awsvalidator_controller_test.go
@@ -50,7 +50,7 @@ var _ = Describe("AWSValidator controller", Ordered, func() {
 			AmiRules: []v1alpha1.AmiRule{
 				{
 					Name:   "AMIRule",
-					AmiIds: []string{"ami-12345678"},
+					AmiIDs: []string{"ami-12345678"},
 					Region: "us-west-2",
 				},
 			},
